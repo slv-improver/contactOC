dbbe3a53c64290325f6813e200051c2bf9838977
slv-improver <k.moret.slv@gmail.com>
2019-09-13 19:46:57 +0100

    amelioration de l'affichage console

--- js/contacts.js
+++ js/contacts.js
@@ -13,23 +13,24 @@ let tableauContact = [];
 tableauContact.push(new Contact("Lévisse", "Carole"));
 tableauContact.push(new Contact("Nelsonne", "Mélodie"));
 
-console.log("Bienvenue dans le gestionnaire des contacts" + "\n" + "1 : Listez les contacts" + "\n" +" 2 : Ajoutez un contact "+ "\n" + " 0: Quittez ");
+console.log("Bienvenue dans le gestionnaire des contacts\n" + "   1 : Listez les contacts\n" + "   2 : Ajoutez un contact\n" + "   0: Quittez\n\n");
 var saisie = "";
 while(saisie !== 0) {
-    saisie = Number(prompt("Choissisez une option"));
+    saisie = Number(prompt("Choissisez une option\n", "1, 2 ou 0"));
     switch(saisie){
         case 1:
-            console.log("Voici la liste de vos contacts")
+            console.log("Voici la liste de vos contacts :\n\n")
             for(const contact of tableauContact){
                 console.log(`Nom : ${contact.nom}, prenom : ${contact.prenom}`);
             }
+            console.log("\n");
             break ;
             case 2:
-                const nom = prompt("Entrez le nom du nouveau contact");
-                const prenom = prompt("Entrez le prenom du nouveau contact");
+                const nom = prompt("Entrez le nom du nouveau contact\n", "Dupont");
+                const prenom = prompt("Entrez le prenom du nouveau contact\n", "Michel");
                 tableauContact.push(new Contact(nom , prenom));
                 break ;          
             }
 }
-            console.log("Au revoir ! ")
+            console.log("Au revoir !")
         
